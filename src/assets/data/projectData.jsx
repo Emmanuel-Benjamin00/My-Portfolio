711faa3f2f8771d771f92fd247d0c44995227b13
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2024-01-05 15:39:17 +0530

    data changed 1/5

--- src/assets/data/projectData.jsx
+++ src/assets/data/projectData.jsx
@@ -9,42 +9,42 @@ const projectsData = [
     {
         img: RestaurantLandingImg,
         title: "Restaurant Landing Page",
-        content: "Collaborated on a feature-packed restaurant website, showcasing skills in web development. Offers user-friendly features like authentication, live order tracking, and admin functionalities. ",
+        content: "A feature-rich restaurant website with user authentication, delivery address management, live order tracking, daily discounts, and admin capabilities for editing the website and monitoring order statuses.",
         view: "https://hotmeals-restaurant.netlify.app/",
         code: "https://github.com/Emmanuel-Benjamin00/restaurant-landing-page"
     },
     {
         img: BlogImg,
         title: "Blog App",
-        content: "Contributed to the development of a versatile blog website that facilitates user authentication, blog creation, and approval processes. Utilized Reactjs for the frontend, Expressjs for the backend, and MongoDB for the database. ",
+        content: "A feature-rich blog website allowing user authentication, blog creation with admin approval, user dashboards, and token expiration, built with Reactjs for the frontend, Expressjs for the backend, and MongoDB for the database.",
         view: "https://bloginme.netlify.app/",
         code: "https://github.com/Emmanuel-Benjamin00/blog-app-frontend"
     },
     {
         img: TodoImg,
         title: "ToDo App",
-        content: "Developed of ToDo website utilizing Reactjs for the frontend, Expressjs with React Redux for the backend, and MongoDB for the database. The platform create, manage, and track todos with features of different tabs for all, pending, and completed todos. ",
+        content: "A ToDo website with intuitive features such as ToDo input, categorized tabs, and status tracking, built using Reactjs for the frontend, Expressjs with React Redux for the backend, and MongoDB for the database.",
         view: "https://i-am-here-to-keep-your-notes.netlify.app/",
         code: "https://github.com/Emmanuel-Benjamin00/Todo-App"
     },
     {
         img: NotesAppImg,
         title: "Notes App",
-        content: "Developed a Notepad front-end application with React and React Redux. This platform is for note creation, editing, and deletion. Utilizing efficient component-based structure, the application ensures seamless note management. ",
+        content: "A user-friendly Notepad website built with React and React Redux, offering features like note creation, note display in a card layout, and  editing and deletion capabilities. The front-end application provides a seamless experience for organizing your notes.",
         view: "https://emmanuel-notes-app-redux.netlify.app/home",
         code: "https://github.com/Emmanuel-Benjamin00/NotesApp-Redux"
     },
     {
         img: TravelLandingImg,
         title: "Travel Landing Page",
-        content: "Developed the front-end for the Bon Voyage Travel Landing Page, a best platform for travel services. The website, built using HTML and CSS, features service categories, top destinations, an easy booking process, and a convenient email subscription option. ",
+        content: "A visually appealing front-end webpage for the Bon Voyage Travel Landing Page, featuring service categories, top destinations, easy booking steps, and a convenient email subscription, built with HTML and CSS.",
         view: "https://unique-lolly-2479b3.netlify.app/",
         code: "https://github.com/Emmanuel-Benjamin00/Travel-Landing-Page"
     },
     {
       img: DiceGameImg,
       title: "Dice Game",
-      content: "Developed an engaging Dice Game using HTML, CSS, and JavaScript with a Flexbox layout. The game follows simple yet exciting rules, including turn-based rolling, scoring, and winning conditions. The first player to reach 30 points is the winner.",
+      content: "A dynamic Dice Game built with HTML, CSS, and JavaScript using Flexbox layout, where players take turns rolling the dice to reach 30 points and reset for a new game.",
       view: "https://dice-winner-2023.netlify.app/",
       code: "https://github.com/Emmanuel-Benjamin00/Dice-Game"
     }
