25569a7db427a8518af9948d2d0948ee2305738e
Emmanuel Benjamin D <Emmanuel.Benjamin@arivu-iq.com>
2024-11-10 15:20:47 +0530

    projects page completed small changes in education and experience

---
+++ src/app/projects/layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const metadata = {
+    title: "Projects",
+    description: "About | Emmanuel Benjamin",
+  };
+
+const layout = ({children}) => {
+  return (   
+    <>
+    {children}
+    </> 
+  )
+}
+
+export default layout
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,7 +5,7 @@ import LiIcon from "../components/LiIcon"
 const Details = ({position, company, companyLink, time, address, work}) =>{
 
     const ref = useRef(null)
-    return <li ref={ref} className='my-8 first-mt-0 last-mt-0 w-[60%] mx-auto flex flex-column items-center justify-between'>
+    return <li ref={ref} className='my-8 first-mt-0 last-mt-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
         <LiIcon reference={ref} />
         <motion.div
             initial={{y:50}}
