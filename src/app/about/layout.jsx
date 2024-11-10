e6bc021842f803a299f9ec93feefb8be6754fc62
Emmanuel Benjamin D <Emmanuel.Benjamin@arivu-iq.com>
2024-11-10 12:31:11 +0530

    Home page, about page with skills, experiemce and education

---
+++ src/app/about/layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const metadata = {
+    title: "About Me",
+    description: "About | Emmanuel Benjamin",
+  };
+
+const layout = ({children}) => {
+  return (   
+    <>
+    {children}
+    </> 
+  )
+}
+
+export default layout
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,27 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+      //  // Or if using `src` directory:
+      //  "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        mont: ['var(--font-mont)'],
       },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+      },
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
+      },
+      backgroundImage:{
+        circularLight : ' repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #f5f5f5 100px);'
+      }
     },
   },
   plugins: [],
