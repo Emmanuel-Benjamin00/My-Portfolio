40f04cf3ff6bf6de7147a0aee621336c8ff0a8be
Emmanuel Benjamin D <Emmanuel.Benjamin@arivu-iq.com>
2025-01-05 00:22:29 +0530

    pushed in this branch for safety of code,  because the original branch changes have to be shown for me in local, use this if the original stage is lost

---
+++ dlt.js
@@ -0,0 +1,69 @@
+'use client'
+import React from 'react';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import AnimatedText from '../components/AnimatedText';
+import HireMe from '../components/HireMe';
+import Image from 'next/image';
+import ProfilePic from '../../public/images/profile/MyImage.png'
+import { LinkArrow, CircularText } from '@/components/Icons';
+import LightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
+import TransitionEffect from '../components/TransitionEffect';
+
+const Home = () => {
+  return (
+    <>
+    <TransitionEffect />
+    <main className="flex items-center text-dark w-full min-h-screen dark:text-light">
+      <Layout className='pt-0 md:pt-16 sm:pt-8'>
+        <div className="flex items-center justify-between w-full lg:flex-col">
+          <div className='w-1/2 md:w-full align-center flex justify-center'>
+          <div className='aspect-square overflow-hidden rounded-full w-4/5 border-primary border-solid border-2'>
+            <Image src={ProfilePic} alt="Emmanuel Benjamin" className='w-full h-auto lg:hidden md:inline-block md:w-full' 
+                   priority
+                  //  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+                   />
+                   </div>
+          </div>
+          <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
+            <AnimatedText text="Turning Vision Into Reality With Code And Design. " className='!text-6xl !text-left
+            xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl
+            ' />
+            <p className='my-4 test-base font-medium md-text-sm sm:text-xs'>
+              As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
+              Explore my latest projects and articles, showcasing my expertise in React.js and web development.
+            </p>
+            <div className='flex items-center self-start mt-2 lg:self-center '>
+              <Link
+                href="/dummy.pdf"
+                target='_blank'
+                className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
+                            hover:bg-light hover:text-dark border-2 border-solid border-transparent
+                            hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
+                            md:p-2 md:px-4 md:text-base '
+                download={true}
+              >
+                Resume
+                <LinkArrow className={"w-6 ml-1"} />
+              </Link>
+              <Link
+                href="mailto:emmanuel26112000@gmail.com"
+                target='_blank'
+                className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'
+              >Contact</Link>
+            </div>
+          </div>
+        </div>
+      </Layout>
+
+      <HireMe />
+
+      <div className='absolute right-8 bottom-8 inline-block w-24 md:hidden'>
+        <Image src={LightBulb} alt="Emmanuel" className='w-full h-auto'/>
+      </div>
+    </main>
+    </>
+  );
+};
+
+export default Home;
