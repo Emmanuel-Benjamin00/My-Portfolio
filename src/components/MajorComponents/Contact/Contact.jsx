9744bc0cfb46e7188dcdf64d633125d68e0a4388
Emmanuel Benjamin D <Emmanuel.Benjamin@arivu-iq.com>
2026-05-23 13:33:01 +0530

    clean contact

--- src/components/MajorComponents/Contact/Contact.jsx
+++ src/components/MajorComponents/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Container, Button, Col, Row } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import "./Contact.css"
